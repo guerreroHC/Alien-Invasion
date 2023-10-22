95e20df93a6f8f603f54a4c9543f68febc08e0ef
guerreroHC <guerreroh2505@gmail.com>
2023-10-22 17:37:29 -0500

    Shooting aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -6,6 +6,7 @@ from ship import Ship
 import game_functions as gf
 from pygame.sprite import Group
 from alien import Alien
+from game_stats import GameStats
 
 
 def run_game():
@@ -15,22 +16,30 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion by Cesar Guerrero")
     
+    # Create an instance to store game statistics
+    stats = GameStats(ai_settings)
+    
+    # Make a ship, a group of bullets, and a group of aliens    
     # Make a ship
     ship = Ship(ai_settings, screen)
     
     # Make a group to store bullets in
     bullets = Group()
     
-    # Make an alien
-    alien = Alien(ai_settings, screen)
+    aliens = Group()
+    
+    # Create the fleet of aliens
+    gf.create_fleet(ai_settings, screen, ship, aliens)
     
     # Start the main loop for the game
     while True:
         gf.check_events(ai_settings,screen,ship,bullets)
-        # Watch for keyboard and mouse events
-        ship.update()
-        gf.update_bullets(bullets)        
-        gf.update_screen(ai_settings,screen,ship,alien,bullets)             
+        
+        if stats.game_active:   
+            ship.update()
+            gf.update_bullets(ai_settings, screen, ship,aliens, bullets) 
+            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)       
+            gf.update_screen(ai_settings,screen,ship,aliens,bullets)             
         
         
 run_game()
--- settings.py
+++ settings.py
@@ -9,12 +9,19 @@ class Settings():
         
         # Ship setting
         self.ship_speed_factor = 0.7
+        self.ship_limit = 3
         
         # Bullet settings
-        self.bullet_speed_factor = 1
+        self.bullet_speed_factor = 3
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = 60,60,60
         self.bullets_allowed = 3
         
+        # Alien settings
+        self.alien_speed_factor = 0.7
+        self.fleet_drop_speed = 4
+        
+        # fleet_direction of 1 represents right, -1 represents left
+        self.fleet_direction = 1
     
